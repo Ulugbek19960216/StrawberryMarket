3bacad3ba960fff8334a6f2ff047a58aa3b89559
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-02-12 13:14:58 +0500

    registrationpost with Axios

--- strawberry/src/compenents/user/Register.jsx
+++ strawberry/src/compenents/user/Register.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button, Typography } from "@mui/material";
 import Logo from "./Partials/Logo";
 import AuthIcons from "./Partials/AuthIcons";
 
 
 function Register() {
+    
     const inputCustom = {
         width: "480px",
         height: "56px",
@@ -26,16 +27,50 @@ function Register() {
         textAlign: "center"
     }
 
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+
+    function handleChange(event){
+
+        const {name, value} = event.target;
+
+        setFormData({...formData, [name]: value});
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("/api/register", formData);
+            if(response) {
+                console.log("Registration successful:", response.data)
+                return redirect("/login");
+            }
+        } catch (error) {
+            console.error("Registration failed: ", error.message );
+        }
+    }
+
   return (
     <Box>
         <Box></Box>
         <Box sx= {{display: "flex", justifyContent: "center", alignItems: "center",backgroundColor: "#F7FAFC", height: "950px"}}>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", backgroundColor: "#FFFFFF", width: "525px", height: "836px", borderRadius: "20px"}}>
                <Logo/>
-                <form action="submit">
+                <form onSubmit={handleSubmit}>
                     <Box style={gap}>
                         <Typography>Email</Typography>
-                        <input style={inputCustom} placeholder='you@example.com'>
+                        <input 
+                            name = "email"
+                            style={inputCustom}
+                            placeholder='you@example.com'
+                            value={formData.email}
+                            onChange={handleChange}
+                            >
                             
                         </input>
                     </Box>
@@ -43,7 +78,15 @@ function Register() {
                         <Typography>
                             Password
                         </Typography>
-                        <input style={inputCustom} placeholder='************'></input>
+                        <input 
+                            name = "password"
+                            style={inputCustom}
+                            placeholder='************'
+                            value={formData.password}
+                            onChange={handleChange}
+                            >
+
+                            </input>
                     </Box>
                     <Box style={gap}>
                         <Typography>
@@ -55,7 +98,7 @@ function Register() {
                         <Typography sx={textStyle}>Use at least 8 characters.</Typography>
                     </Box>
                     <Box style={gap}> 
-                        <Button variant="contained" sx={{ width: "502px", height: "46px", borderRadius: "12px", backgroundColor: "#F20D33"}}>Sign up</Button>
+                        <Button variant="contained" type="submit" sx={{ width: "502px", height: "40px", '&:hover': {backgroundColor: "#F20D33"}, borderRadius: "12px", backgroundColor: "#F20D33"}}>Sign up</Button>
                     </Box>
                     <Box style={gap} sx={textCenter}>
                         <Typography sx={textStyle}>By creating an account, you agree to strawberry's</Typography>
