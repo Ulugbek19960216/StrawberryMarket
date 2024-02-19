8280fd045c9d0c3e3d47508bd838711dee68929e
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-02-19 15:25:36 +0500

    Updated component style code by using consistenly MUI style

--- src/compenents/user/ConfirmationEmail.jsx
+++ src/compenents/user/ConfirmationEmail.jsx
@@ -50,62 +50,60 @@ function ConfirmationEmail() {
         } 
        }, [code, otp]);
 
-    const container = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#F0F2F5",
-        boxSizing: "border-box"
-    }
-
-    const wrapper = {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        height: "450px",
-        width: "512px",
-        borderRadius: "20px",
-        backgroundColor: "white",
-        padding: "10px"
-
-    }
-    
-    const title = {
-        marginTop: "10px",
-        fontSize: "28px",
-        fontWeight: "bold",
-        fontFamily: "Manrope",
-        textAlign: "center",
-        lineHeight: "35px",
-        letterSpacing: "1px"
-    }
-
-    const inputBox = {
-        textAlign: "center",
-        fontSize: "23px",
-        width: "48px",
-        height: "56px",
-        borderRadius: "12px",
-        marginLeft: "16px",
-        backgroundColor: "#F0F2F5",
-        padding: "8px, 8px",
-        border: "2px solid #DBE0E5 "
-    }
-
-
   return (
-    <Box sx={container}>
-        <Box sx={wrapper}>
+    <Box 
+        sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            backgroundColor: "#F0F2F5",
+            boxSizing: "border-box"
+        }}>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "450px",
+            width: "512px",
+            borderRadius: "20px",
+            backgroundColor: "white",
+            padding: "10px"}}>
+
             <Box sx={{height: "100px", width: "500px", marginBottom: "45px"}}>
-                <Typography sx ={title}>Enter the 4-digit code sent to you at your*****@gmail.com <br/> address</Typography>
+                <Typography 
+                sx ={{
+                    marginTop: "10px",
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                    fontFamily: "Manrope",
+                    textAlign: "center",
+                    lineHeight: "35px",
+                    letterSpacing: "1px"}}>
+                    Enter the 4-digit code sent to you at your*****@gmail.com <br/> address</Typography>
             </Box>
-            <Box sx={{width: "512", height: "80px", display: "flex", marginBottom: "10px", alignItem: "center", justifyConten: "center" }}>
+            <Box 
+                sx={{
+                    width: "512",
+                    height: "80px",
+                    display: "flex",
+                    marginBottom: "10px",
+                    alignItem: "center",
+                    justifyConten: "center"}}>
 
                 {code.map((digit, index) => (
                     <input
                         key={index}
-                        style={inputBox}
+                        style={{
+                            textAlign: "center",
+                            fontSize: "23px",
+                            width: "48px",
+                            height: "56px",
+                            borderRadius: "12px",
+                            marginLeft: "16px",
+                            backgroundColor: "#F0F2F5",
+                            padding: "8px, 8px",
+                            border: "2px solid #DBE0E5 "}}
                         type="text"
                         maxLength={1}
                         value={digit}
@@ -117,17 +115,49 @@ function ConfirmationEmail() {
                 ))}
             </Box>
             <p className={`text-lg text-white mt-4 ${otpError ? 'error-show' : ''}`}>{otpError}</p>
-            <Box sx={{width: "218px", height: "26px", marginBottom: "40px"}}>
-                 <Typography sx={{color: "red", fontWeight: "bold"}}>05:00</Typography>
+            <Box sx={{
+                width: "218px",
+                height: "26px",
+                marginBottom: "40px"}}>
+                <Typography sx={{
+                    color: "red",
+                    fontWeight: "bold"}}>05:00</Typography>
             </Box>
 
-            <Box sx={{display: "flex", justifyContent: "space-between", width: "500px", height: "25px", marginBottom: "35px"}}>
-                <Box><Typography sx={{color: "#637587", fontSize: "14px", fontFamily: "Manrope" }}>I didn't receive a code</Typography></Box>
-                <Box> <Typography sx={{color: "#637587", fontSize: "14px", fontFamily: "Manrope" }}>Resend</Typography></Box>
+            <Box sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "500px",
+                height: "25px",
+                marginBottom: "35px"}}>
+                <Box><Typography sx={{
+                    color: "#637587",
+                    fontSize: "14px",
+                    fontFamily: "Manrope" }}>I didn't receive a code</Typography></Box>
+                <Box> <Typography sx={{
+                    color: "#637587",
+                    fontSize: "14px",
+                    fontFamily: "Manrope" }}>Resend</Typography></Box>
             </Box>
-            <Box sx={{display: "flex", justifyContent: "space-between", width: "500px", height: "48px"}}>
-                <Button  sx={{ '&:hover': {backgroundColor: '#ffffff', }, backgroundColor: "#F0F2F5", color: "#121417", fontWeight: "Bold", width: "110px", borderRadius: "12px"}}variant="contained">Previous</Button>
-                <Button sx={{  '&:hover': {backgroundColor: '#ffffff', }, backgroundColor: "#F0F2F5", color: "#121417", fontWeight: "Bold", width: "84px", borderRadius: "12px"}} variant="contained">Next</Button>
+            <Box sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "500px",
+                height: "48px"}}>
+                <Button  sx={{ 
+                    '&:hover': {backgroundColor: '#ffffff', },
+                    backgroundColor: "#F0F2F5",
+                    color: "#121417",
+                    fontWeight: "Bold",
+                    width: "110px",
+                    borderRadius: "12px"}}variant="contained">Previous</Button>
+                <Button sx={{  
+                    '&:hover': {backgroundColor: '#ffffff', },
+                    backgroundColor: "#F0F2F5",
+                    color: "#121417",
+                    fontWeight: "Bold",
+                    width: "84px",
+                    borderRadius: "12px"}} variant="contained">Next</Button>
             </Box>
 
         </Box>
