1b6348094ff4daad7f50d82c58979d71d5e6eba5
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-02-19 15:09:29 +0500

    Added OPT functionality

--- src/compenents/user/ConfirmationEmail.jsx
+++ src/compenents/user/ConfirmationEmail.jsx
@@ -1,21 +1,69 @@
 import { Typography, Box, Button } from '@mui/material'
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react'
+import axios from "axios";
 
 
 function ConfirmationEmail() {
+    const [otp, setOTP] = useState("");
+    const [code, setCode] = useState(["", "", "", ""]);
+    const [otpError, setOtpError] = useState(null);
+    const otpBoxReference = useRef([]);
+    const numberOfDigits = code.length;
+
+
+
+    const fetchOTP = async () => {
+        try{
+            const response = await axios.get("api/otp");
+            setOTP(response.data);
+        } catch (error) {
+            console.error("Error fetching OTP: ", error);
+        }
+    };
+
+    const handleInputChange = (index, value) => {
+        if (value === "" || (value.length === 1 && /^\d$/.test(value))) {
+            const newCode = [...code];
+            newCode[index] = value;
+            setCode(newCode);
+        }
+
+        if(value && index < numberOfDigits-1){
+        otpBoxReference.current[index + 1].focus()
+        }
+    };
+
+    function handleBackspaceAndEnter(e, index) {
+        if(e.key === "Backspace" && !e.target.value && index > 0){
+          otpBoxReference.current[index - 1].focus()
+        }
+        if(e.key === "Enter" && e.target.value && index < numberOfDigits-1){
+          otpBoxReference.current[index + 1].focus()
+        }
+      }
+
+      useEffect(() => { 
+        if(code.join("") !== "" && code.join("") !== otp){
+          setOtpError("❌ Wrong OTP Please Check Again")
+        }else{
+          setOtpError(null)
+        } 
+       }, [code, otp]);
+
     const container = {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        backgroundColor: "#F0F2F5"
+        backgroundColor: "#F0F2F5",
+        boxSizing: "border-box"
     }
 
     const wrapper = {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        height: "425px",
+        height: "450px",
         width: "512px",
         borderRadius: "20px",
         backgroundColor: "white",
@@ -34,13 +82,17 @@ function ConfirmationEmail() {
     }
 
     const inputBox = {
+        textAlign: "center",
+        fontSize: "23px",
         width: "48px",
         height: "56px",
         borderRadius: "12px",
         marginLeft: "16px",
         backgroundColor: "#F0F2F5",
-        padding: "8px, 8px"
+        padding: "8px, 8px",
+        border: "2px solid #DBE0E5 "
     }
+
 
   return (
     <Box sx={container}>
@@ -48,17 +100,28 @@ function ConfirmationEmail() {
             <Box sx={{height: "100px", width: "500px", marginBottom: "45px"}}>
                 <Typography sx ={title}>Enter the 4-digit code sent to you at your*****@gmail.com <br/> address</Typography>
             </Box>
-            <Box sx={{width: "512", height: "80px", display: "flex", marginBottom: "10px"}}>
-                <Box sx={inputBox}></Box>
-                <Box sx={inputBox}></Box>
-                <Box sx={inputBox}></Box>
-                <Box sx={inputBox}></Box>
+            <Box sx={{width: "512", height: "80px", display: "flex", marginBottom: "10px", alignItem: "center", justifyConten: "center" }}>
+
+                {code.map((digit, index) => (
+                    <input
+                        key={index}
+                        style={inputBox}
+                        type="text"
+                        maxLength={1}
+                        value={digit}
+                        onChange={(e) => handleInputChange(index, e.target.value)}
+                        onKeyUp={(e)=> handleBackspaceAndEnter(e, index)}
+                        ref={(reference) => (otpBoxReference.current[index] = reference)}
+                    >
+                    </input>
+                ))}
             </Box>
-            <Box sx={{width: "218px", height: "26px", marginBottom: "45px"}}>
+            <p className={`text-lg text-white mt-4 ${otpError ? 'error-show' : ''}`}>{otpError}</p>
+            <Box sx={{width: "218px", height: "26px", marginBottom: "40px"}}>
                  <Typography sx={{color: "red", fontWeight: "bold"}}>05:00</Typography>
             </Box>
 
-            <Box sx={{display: "flex", justifyContent: "space-between", width: "500px", height: "25px", marginBottom: "40px"}}>
+            <Box sx={{display: "flex", justifyContent: "space-between", width: "500px", height: "25px", marginBottom: "35px"}}>
                 <Box><Typography sx={{color: "#637587", fontSize: "14px", fontFamily: "Manrope" }}>I didn't receive a code</Typography></Box>
                 <Box> <Typography sx={{color: "#637587", fontSize: "14px", fontFamily: "Manrope" }}>Resend</Typography></Box>
             </Box>
