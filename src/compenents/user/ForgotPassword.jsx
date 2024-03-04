1d18ed4416fb283b54ed3870f6da3f6b51143dac
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-03-04 14:20:20 +0500

    Added validateEmail function

--- src/compenents/user/ForgotPassword.jsx
+++ src/compenents/user/ForgotPassword.jsx
@@ -1,15 +1,33 @@
 import { Typography, Box, Button, Paper } from '@mui/material'
-import { NavLink } from 'react-router-dom';
 import React, {useState} from 'react'
 
 function ForgotPassword() {
     const [userEmail, setUserEmail] = useState(""); 
+    const [error, setError] = useState("");
 
+
+    const validateEmail = (email) => {
+        const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        return isValid;
+    }
 
     function handleChange(event) {
         const value = event.target.value;
         setUserEmail(value)
+
     }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(!validateEmail(userEmail)) {
+            setError("Invalid email format");
+        } else {
+            setError("");
+        }
+    }
+
   return (
     <Box sx={{
         display: "flex",
@@ -19,13 +37,13 @@ function ForgotPassword() {
         backgroundColor: "#F7FAFC"
     }}>
         <Box></Box>
-        <Paper elevation="4" sx={{
+        <Paper elevation={4} sx={{
             display: "flex",
             flexDirection: "column",
             padding: "15px",
             backgroundColor: "#FFFFFF",
             height: "470px",
-            width: "512px",
+            width: "515px",
             borderRadius: "20px",
             fontFamily: "Manrope"
         }}>
@@ -58,52 +76,57 @@ function ForgotPassword() {
                     Enter your email and we'll send you a link to reset your <br/> password.
                 </Typography>
             </Box>
-            <Box sx={{
-                width: "512px",
-                height: "112px",
-                padding: "10px"
-            }}>
-                <Typography sx={{
-                    fontWeight: "700",
-                    fontSize: "16px",
-                    lineHeight: "24px",
-                    fontFamily: "Manrope",
-                    marginBottom: "10px"
-                }}>Email</Typography>
+            <form onSubmit={handleSubmit}>
+                <Box sx={{
+                    height: "112px",
+                    width:"500px",
+                    padding: "10px"
+                }}>
+                    <Typography sx={{
+                        fontWeight: "700",
+                        fontSize: "16px",
+                        lineHeight: "24px",
+                        fontFamily: "Manrope",
+                        marginBottom: "10px"
+                    }}>Email</Typography>
                 
-                <input 
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    value = {userEmail}
-                style={{
-                    width: "480px",
-                    height: "56px",
-                    padding: "0 10px",
-                    fontFamily: "Manrope",
-                    fontSize: "18px",
-                    borderRadius: "12px",
-                    border: "3px solid #F1F1F1"
-                }}></input>
-                <Typography></Typography>
-                
-            </Box>
-            <Box sx={{
-                padding: "10px",
-            }}>
-                <Button variant='contained' sx={{
-                    width: "485px",
-                    height: "48px",
-                    borderRadius: "12px",
-                    backgroundColor: "#F20D33",
-                    textTransform: "none",
-                    fontSize: "16px",
-                    fontWeight: "bold",
-                    letterSpacing: "0.24px",
-                    '&:hover': {backgroundColor: "#F20D33"}
-                }}><NavLink style={{textDecoration: "none", color: "white"}} to="/verifyEmail" >Recovery</NavLink></Button>
+                    <input 
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        value = {userEmail}
+                    style={{
+                        width: "478px",
+                        height: "56px",
+                        padding: "0 10px",
+                        fontFamily: "Manrope",
+                        fontSize: "18px",
+                        borderRadius: "12px",
+                        border: "3px solid #F1F1F1"
+                    }}></input>
+                    {error && <Typography sx={{color: "red", fontFamily: "Manrope", m: "5px"}}>{error}</Typography>}
+                    
+                </Box>
+                <Box sx={{
+                    padding: "10px",
+                }}>
+                    <Button 
+                        variant='contained'
+                        type = "submit"
+                        sx={{
+                        width: "502px",
+                        height: "48px",
+                        borderRadius: "12px",
+                        backgroundColor: "#F20D33",
+                        textTransform: "none",
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        letterSpacing: "0.24px",
+                        '&:hover': {backgroundColor: "#F20D33"}
+                    }}>Recovery</Button>
 
-            </Box>
+                </Box>
+            </form>
         </Paper>
     </Box>
   )
